4ec5fa469758c32d9bbe9a5e1dbb5e3e77aab0dc
Anishkannan9566 <123641546+Anishkannankavi@users.noreply.github.com>
2023-02-01 11:41:18 +0530

    Add files via upload
    week2.day2

---
+++ day2/Factorial.java
@@ -0,0 +1,15 @@
+package week2.day2;
+
+public class Factorial {
+	
+		public static void main(String[] args) {
+			int a=5;
+			int b=1;
+			for (int i = 1; i <= a; i++) {
+				b=b*i;
+			}
+			System.out.println(b);
+		}
+
+
+}
